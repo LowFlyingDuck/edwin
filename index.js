8921a5738b04c0a5612c19e8bd1b84b0f0b7d6b1
data4040 <datateam@gmx.de>
2020-08-20 22:04:54 +0200

    main

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const express = require('express');
-const app = express();
+const fs = require('fs');
+const { createGzip } = require('zlib');
+const staticServe = fs.readdirSync('./public');
+console.log(staticServe);
 
-app.listen(80);
-app.use(express.static('./public'));
+function provide(url, response, compress) {
+  if (staticServe.includes(url.slice(1, url.length))) {
+    try {
+      let s = fs.createReadStream('./public' + url);
+      if (compress) {
+        let encoder = createGzip();
+        response.setHeader('Content-Encoding', 'gzip');
+        s.pipe(encoder);
+        encoder.pipe(response);
+        encoder.on('close', () => response.end());
+      } else { s.pipe(response); s.on('close', () => response.end()); };
+      return true;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const server = require('http').createServer((request, response) => {
+  response.setHeader('Server', 'Finn Lyonn WebServer | NodeJS');
+  switch (request.url) {
+    case '/':
+      provide('/index.html', response, request.headers['accept-encoding'].replace(/ /g, '').split(','));
+      break;
+    default:
+      if (staticServe.includes(request.url.slice(1, request.url.length - 1))) {
+        provide(request.url, response, request.headers['accept-encoding'].replace(/ /g, '').split(','));
+      }
+      provide(request.url, response, request.headers['accept-encoding'].replace(/ /g, '').split(',').includes('gzip'));
+      break;
+  }
+})
+server.listen(80);
